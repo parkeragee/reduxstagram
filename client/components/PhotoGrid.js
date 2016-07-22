cc3a34a6a228f62a6e214cc05ee3136cc43a5147
Parker Agee <pagee@3442452.local>
2016-07-22 12:57:17 -0500

    Removing node_modules

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import { Link } from 'react-router';
+
+const PhotoGrid = React.createClass({
+	render() {
+		return (
+			<div className="photo-grid">
+			Photo
+			</div>
+		)
+	}
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import { Link } from 'react-router';
+
+const Single = React.createClass({
+	render() {
+		return (
+			<div className="single-photo">
+			Single
+			</div>
+		)
+	}
+});
+
+export default Single;
